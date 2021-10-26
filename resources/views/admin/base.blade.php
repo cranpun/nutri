8e1b9ca74466dc0432bcfe8558702dda8c7d75b0
cranpun <cranpun@yahoo.co.jp>
2021-10-26 16:23:33 +0900

    ロゴ、日付範囲の調整

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -12,6 +12,9 @@
     <header id="header" class="d-none-print">
         <nav class="navbar has-background-primary-light" role="navigation">
             <section class="navbar-brand">
+                <a class="navbar-item" href="{{ url('/') }}">
+                    <img src="nutri.svg" style="height: 100%">
+                </a>
                 <a id="burger" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
                     data-target="navbar-topnav">
                     <span aria-hidden="true"></span>
--- resources/views/admin/menu/index/extend.blade.php
+++ resources/views/admin/menu/index/extend.blade.php
@@ -7,9 +7,9 @@ use \App\Http\Controllers\Admin\Menu\MenuController;
 
 $modalaction = "extend";
 $postdata = [
-    MenuController::$index_NAME_EDATE => \Carbon\Carbon::today()->addDays(14)->format("Y-m-d"),
-    MenuController::$index_NAME_SDATE => \Carbon\Carbon::today()->addDays(-14)->format("Y-m-d"),
+    MenuController::$index_NAME_EDATE => \Carbon\Carbon::today()->addDays(30)->format("Y-m-d"),
+    MenuController::$index_NAME_SDATE => \Carbon\Carbon::today()->addDays(-30)->format("Y-m-d"),
 ];
 $posturl = route('admin-menu-index', $postdata);
 ?>
-<a class="button" href="{{ $posturl }}" id="act-{{ $modalaction }}">前後2週間</a>
+<a class="button" href="{{ $posturl }}" id="act-{{ $modalaction }}">前後30日</a>
