41023ff8958bb11fa4d68e93f4bb6ed2cb7bf0e2
cranpun <cranpun@yahoo.co.jp>
2021-10-26 16:40:46 +0900

    範囲調整

--- resources/views/admin/menu/index/extend.blade.php
+++ resources/views/admin/menu/index/extend.blade.php
@@ -8,8 +8,8 @@ use \App\Http\Controllers\Admin\Menu\MenuController;
 $modalaction = "extend";
 $postdata = [
     MenuController::$index_NAME_EDATE => \Carbon\Carbon::today()->addDays(30)->format("Y-m-d"),
-    MenuController::$index_NAME_SDATE => \Carbon\Carbon::today()->addDays(-30)->format("Y-m-d"),
+    MenuController::$index_NAME_SDATE => \Carbon\Carbon::today()->addDays(-90)->format("Y-m-d"),
 ];
 $posturl = route('admin-menu-index', $postdata);
 ?>
-<a class="button" href="{{ $posturl }}" id="act-{{ $modalaction }}">前後30日</a>
+<a class="button" href="{{ $posturl }}" id="act-{{ $modalaction }}">過去献立</a>
