a2e651ada3b6ba1d4ac0a163428bfccd7fe8ac05
cranpun <cranpun@yahoo.co.jp>
2022-04-07 11:01:21 +0900

    過去献立：表示範囲を拡張

--- resources/views/admin/menu/index/extend.blade.php
+++ resources/views/admin/menu/index/extend.blade.php
@@ -8,7 +8,7 @@ use \App\Http\Controllers\Admin\Menu\MenuController;
 $modalaction = "extend";
 $postdata = [
     MenuController::$index_NAME_EDATE => \Carbon\Carbon::today()->addDays(30)->format("Y-m-d"),
-    MenuController::$index_NAME_SDATE => \Carbon\Carbon::today()->addDays(-90)->format("Y-m-d"),
+    MenuController::$index_NAME_SDATE => \Carbon\Carbon::today()->addDays(-370)->format("Y-m-d"),
 ];
 $posturl = route('admin-menu-index', $postdata);
 ?>
