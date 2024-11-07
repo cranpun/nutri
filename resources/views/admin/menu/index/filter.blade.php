85c1de9b422e948be40cf7e263b2261a5eea363a
cranpun <cranpun@yahoo.co.jp>
2024-11-07 10:59:55 +0900

    updateと微修正

--- resources/views/admin/menu/index/filter.blade.php
+++ resources/views/admin/menu/index/filter.blade.php
@@ -8,14 +8,14 @@ use \App\Http\Controllers\Admin\Menu\MenuController;
 $modalaction = "index";
 $posturl = route('admin-menu-index');
 ?>
-    <a class="button" id="act-{{ $modalaction }}-open" onclick="document.querySelector('#modal-{{ $modalaction }}').classList.add('is-active');">フィルター</a>
+    <a class="button" id="act-{{ $modalaction }}-open" onclick="document.querySelector('#modal-{{ $modalaction }}').classList.add('is-active');">日付範囲</a>
     <div id="modal-{{ $modalaction }}" class="modal">
         <div class="modal-background" onclick="document.querySelector('#modal-{{ $modalaction }}').classList.remove('is-active')"></div>
         <div class="modal-content">
             <div class="box">
                 <form name="form-{{ $modalaction }}" id="form-{{ $modalaction }}" method="GET" action="{{ $posturl }}" class="">
-                    <x-myinput :field="MenuController::$index_NAME_EDATE" label="終了日" type="date" :defval="$srch[MenuController::$index_NAME_EDATE]" placeholder="" />
                     <x-myinput :field="MenuController::$index_NAME_SDATE" label="開始日" type="date" :defval="$srch[MenuController::$index_NAME_SDATE]" placeholder="" />
+                    <x-myinput :field="MenuController::$index_NAME_EDATE" label="終了日" type="date" :defval="$srch[MenuController::$index_NAME_EDATE]" placeholder="" />
                     <fieldset class="field">
                         <button type="submit" id="act-{{ $modalaction }}" class="button">読み込み</button>
                     </fieldset>
